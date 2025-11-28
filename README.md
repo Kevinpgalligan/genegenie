599b748f035b1730ae36d3316451b1c28d873ca1
Kevin Galligan <kevin.galligan@manakau.com>
2025-11-28 21:56:00 +0000

    Homepage and people index.

--- README.md
+++ README.md
@@ -7,3 +7,27 @@ This will be very specific to how I use the Gramps software, and the conventions
 * Gramps 5.1 (haven't tested with other versions).
 * The Gramps Python package should be on your Python path. I achieved this by installing Gramps through my Ubuntu package manager: `apt install gramps`.
 * Flask (Python web server... I will eventually freeze the requirements in a requirements.txt).
+
+### TODO
+* Fix flaky close.
+```
+^CTraceback (most recent call last):
+  File "/home/kg/proj/genegenie/app.py", line 82, in <module>
+    app.run(port=8000)
+  File "/home/kg/proj/genegenie/app.py", line 79, in main
+    sys.exit(1)
+  File "/home/kg/proj/genegenie/app.py", line 52, in close_db
+    state.get_database().close()
+  File "/usr/lib/python3/dist-packages/gramps/gen/db/generic.py", line 739, in close
+    self._close()
+  File "/usr/lib/python3/dist-packages/gramps/plugins/db/dbapi/dbapi.py", line 199, in _close
+    self.dbapi.close()
+  File "/usr/lib/python3/dist-packages/gramps/plugins/db/dbapi/sqlite.py", line 190, in close
+    self.__connection.close()
+sqlite3.ProgrammingError: SQLite objects created in a thread can only be used in that same thread. The object was created in thread id 139822815704640 and this is thread id 139822887157760.
+```
+* Bigger text, higher contrast link colour.
+* More columns in People page, e.g. birth date, death date, ... (check Gramps's People tab).
+* Fill out the Person page.
+* Sources page? Events page? Tree browser?
+* Aesthetic polishing.
--- app.py
+++ app.py
@@ -2,43 +2,85 @@ import argparse
 from pathlib import Path
 import sys
 
-from flask import Flask
+from flask import Flask, render_template
 from gramps.gen.dbstate import DbState
 from gramps.gen.lib import Person
 from gramps.gen.db.utils import make_database
 
-# TODO:
-# Implement the various pages.
-#   a) /person/<id>
-#   b) /people.html   [list of all people]
-#   etc.
-
 app = Flask(__name__)
+
+people = None
+db_path = None
+state = None
+db_ready = False
+
+def set_db_path(path: Path):
+    global db_path
+    db_path = path
+
+def get_people():
+    global people
+    if people is None:
+        people = sorted(load_people(), key=get_person_name)
+        print("Loaded", len(people), "people")
+    return people
+
+def get_person_name(person):
+    return person.get_primary_name().get_name()
+
+def load_people():
+    global state
+    init_db()
+    return [Person(person_data) 
+            for handle, person_data
+            in state.db.get_person_cursor()]
+
+def init_db():
+    global db_path, state, db_ready
+    if not state:
+        state = DbState()
+    if not db_ready:
+        state.change_database(make_database("sqlite"))
+        def dummy_callback(v): pass
+        state.db.load(db_path, dummy_callback, "r")
+        db_ready = True
+        print("Database loaded successfully from", db_path)
+    
+def close_db():
+    global state
+    if state and db_ready:
+        state.get_database().close()
+
+@app.route("/")
+@app.route("/home.html")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/people.html")
+def people_page():
+    return render_template("people.html", people=get_people())
+
+@app.route("/person/<person_id>.html")
+def person_page(person_id):
+    return render_template("person.html", person_id=person_id)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("dbpath", type=str)
-
+    parser.add_argument(
+        "--dbpath",
+        type=str,
+        required=False,
+        help="path to Gramps database, e.g. ~/.gramps/grampsdb/<tree-id>")
     args = parser.parse_args()
-    dbpath = Path(args.dbpath)
+    if args.dbpath:
+        dbpath = Path(args.dbpath)
+        if not dbpath.exists():
+            print("Invalid path to grampsdb:", dbpath, file=sys.stderr)
+            sys.exit(1)
+        set_db_path(dbpath)
 
-    if not dbpath.exists():
-        print("Invalid path to grampsdb:", dbpath, file=sys.stderr)
-        sys.exit(1)
-    
-    state = DbState()
-    state.change_database(make_database("sqlite"))
-    def dummy_callback(v): pass
-    state.db.load(args.dbpath, dummy_callback, "r")
-
-    for i, (handle, person_data) in enumerate(state.db.get_person_cursor()):
-        person = Person(person_data)
-        name = person.get_primary_name()
-        print(i, name.get_name())
-
-    state.get_database().close()
-
-    #app.run(port=8000)
+    app.run(port=8000)
+    close_db()
 
 if __name__ == "__main__":
     main()
